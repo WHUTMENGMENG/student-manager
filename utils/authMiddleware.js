e1f65cd853037f6fb83908a22f356650f9a0457e
前端小菜鸟 <718781500@qq.com>
2020-01-14 17:30:41 +0800

    优化路径不匹配返回权限错误的问题

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -18,14 +18,42 @@ const authorizition = (req, res, next) => {
                 res.send({ status: 0, state: false, msg: "校验失败" })
             } else {
                 if (req.path !== "/verify") {
+                    //首先校验路径是否合法 不合法返回404
+                    let allRoutes = [
+                        "/getloginlog",
+                        "/students/getclasses",
+                        "/students/getstulist",
+                        "students/addstu",
+                        "/students/delstu",
+                        "/students/updatestu",
+                        "/students/searchstu",
+                        "/students/uploadStuAvatar",
+                        "/users/getAllUers",
+                        "/users/register",
+                        "/users/login",
+                        "/users/sigout",
+                        "/users/uploadAvatar",
+                        "/verify",
+                        "/users/updatePassword",
+                        "/permission/addrole",
+                        "/permission/getrole",
+                        "/permission/getMenuList"
+                    ]
                     //校验访问的路径是否合法(是否有权限)
-                    // console.log(req.session.userInfo, "222222")
-                    let isAuth = req.session.userInfo.rows.some(item => item === req.path)
-                    if (isAuth) {
-                        next()
+                    let isAccessRoutes = allRoutes.some(routes => req.path === routes)
+                    if (isAccessRoutes) {//路径合法则进行权限路径校验和当前用户的权限路径作对比
+                        // console.log(req.session.userInfo, "222222")
+                        let isAuth = req.session.userInfo.rows.some(item => item === req.path)
+                        if (isAuth) {
+                            next()
+                        } else {
+                            res.send({ status: '403', code: "10026", state: false, msg: "not permitted 没有权限" })
+                        }
                     } else {
-                        res.send({ status: '403', code: "10026", state: false, msg: "not permitted 没有权限" })
+                        res.status(404)
+                        res.send({ status: 404, msg: "接口地址错误" })
                     }
+
                 } else {
                     res.send({ status: 1, state: true, msg: "校验成功", decode })
                 }
