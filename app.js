44741d98d7bea6dbb234221bfa559e62bc5e4708
前端小菜鸟 <718781500@qq.com>
2020-07-19 22:37:55 +0800

    修复小错误

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "authorization,Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
+    // res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
 //session模块
