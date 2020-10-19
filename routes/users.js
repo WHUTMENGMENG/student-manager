f4ccfabe2fb694a7311de5c6a611c5e99384b41a
前端摸鱼欧阳锋 <718781500@qq.com>
2020-10-19 21:01:00 +0800

    '增加验证码功能'

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,49 @@
 const express = require("express");
 
 const router = express.Router();
-
+var svgCaptcha = require('svg-captcha');
 const { register, login, uploadAvatar, updatePassword, getScancodeCtr, getAllUsers, wechatLoginCtr, wechatCallBackCtr } = require("../controller/usersController")
 
 const uploads = require("../middleware/multer")
 console.log(uploads)
 //注册
 module.exports = function (io) {
+
+    function createCaptcha(req, res) {
+        var codeConfig = {
+            size: 5,// 验证码长度
+            ignoreChars: '0o1i', // 验证码字符中排除 0o1i
+            noise: 2, // 干扰线条的数量
+            height: 44
+        }
+        var captcha = svgCaptcha.create(codeConfig);
+        req.session.captcha = captcha.text.toLowerCase(); //存session用于验证接口获取文字码
+        console.log(req.session.captcha);
+        
+        var codeData = {
+            img: captcha.data
+        }
+        res.send(codeData);
+    }
+
+    //获取验证码
+    router.get("/getCaptcha", createCaptcha)
+
+    //刷新验证码
+    router.get("/refreshCaptcha", createCaptcha)
+
+    //校验验证码
+    router.get("/verifyCaptcha", (req, res) => {
+        console.log(req.query.captcha);
+        console.log(req.session.captcha);
+
+
+        if (req.query.captcha == req.session.captcha) {
+            res.send({ state: true, status: 200, msg: "验证码正确" })
+        } else {
+            res.send({ state: false, status: 3004, msg: "验证码错误" })
+        }
+    })
 
     router.post("/register", register)
 
--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -7,7 +7,10 @@ const authorizition = (req, res, next) => {
         "/students/uploadStuAvatar",
         "/users/wechatLogin",
         "/users/wechatCallBack",
-        "/users/getScancode"
+        "/users/getScancode",
+        "/users/getCaptcha",
+        "/users/verifyCaptcha",
+        "/users/refreshCaptcha",
     ];
     let matchRes = rowPath.some(item => item === req.path)
     if (matchRes || /\/avatar\/.*/.test(req.path)) {
@@ -23,7 +26,7 @@ const authorizition = (req, res, next) => {
         let secrect = "YOU_PLAY_BASKETBALL_LIKE_CAIXUKUN"
         jwt.verify(token, secrect, (err, decode) => {
             if (err) {
-                res.send({ status: 0, code:"1004",state: false, msg: "校验失败" })
+                res.send({ status: 0, code: "1004", state: false, msg: "校验失败" })
             } else {
                 if (req.path !== "/verify") {
                     //首先校验路径是否合法 不合法返回404
@@ -52,7 +55,7 @@ const authorizition = (req, res, next) => {
                     //校验访问的路径是否合法(是否有权限)
                     let isAccessRoutes = allRoutes.some(routes => req.path === routes)
                     if (isAccessRoutes) {//路径合法则进行权限路径校验和当前用户的权限路径作对比
-                         console.log(req.session.userInfo, "222222")
+                        console.log(req.session.userInfo, "222222")
                         let isAuth = req.session.userInfo.rows.some(item => item === req.path)
                         if (isAuth) {
                             next()
