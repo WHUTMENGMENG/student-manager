d24abfffd85e25c092bf8fce2402bfa866d5cd68
前端摸鱼欧阳锋 <718781500@qq.com>
2020-12-30 19:09:02 +0800

    完成订单到支付主逻辑80%

---
+++ utils/xml2json.js
@@ -0,0 +1,21 @@
+const parseString = require("xml2js").parseString;
+
+const parseXML = xml => {
+    let res;
+    parseString(xml, function (err, result) {
+        if (err) {
+            console.log(err);
+            return false
+        }
+        res = result
+
+    });
+    res = { ...res.xml }
+    for (let k in res) {
+        console.log(k);
+        res[k] = res[k] + ""
+        res[k] = res[k].replace(/[\[\]]./g, "")
+    }
+    return res
+}
+module.exports = parseXML
