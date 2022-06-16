2771f0d9dfd82af2e9afb30a431fdcdb6b6f70f9
前端摸鱼欧阳锋 <718781500@qq.com>
2022-06-16 09:23:28 +0800

    修复一些Bug

--- routes/sms.js
+++ routes/sms.js
@@ -1,5 +1,6 @@
 //1.引入express
 let express = require("express");
+const { random } = require("mongoose/lib/utils");
 
 //引入手机登入的sdk
 const smsClient = require("../controller/smsController")
@@ -8,27 +9,33 @@ const smsClient = require("../controller/smsController")
 
 let router = express.Router()
 
-router.post("/send", function (req, res) {
-    let { phoneNumber } = req.body;
-    if (!phoneNumber) {
-        res.send({ state: false, status: 3004, msg: "请输入手机号" })
-        return
-    }
+function randomCode(){
     let num = '0123456789';
     let code = ""
     for (var i = 0; i < 5; i++) {
         let random = Math.floor(Math.random() * 10);
         code += num[random]
     }
+    return code
+}
+
+router.post("/send", function (req, res) {
+    let { phoneNumber } = req.body;
+    if (!phoneNumber) {
+        res.send({ state: false, status: 3004, msg: "请输入手机号" })
+        return
+    }
+   
     //短信选项
     let options = {
         signName: "千锋摸鱼欧阳锋管理系统",
         templateCode: "SMS_239326920",
         phoneNumbers: phoneNumber,
-        code
+        code:randomCode()
     }
+    req.options = options;
     // console.log(code)
-    smsClient.default.send(req, res, options)
+    smsClient.default.send(req, res, req.options)
 })
 
 module.exports = router
